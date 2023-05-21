2a793d67859031a223e8c6de3d8acf03bbf32ccd
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-21 22:20:30 +0800

    creating the ui class and run it to the calculator oop

--- calculator_oop.py
+++ calculator_oop.py
@@ -1,2 +1,9 @@
 #Import the module and files
+from user_interface import UserInterface
+from calculator_class import Calculator
+
+ui = UserInterface()
+calcu = Calculator()
+
+ui.intro()
 #Create an instance and run it
--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,15 @@
+
+import pyfiglet
+import time
 #Define User interface class
-#Display the welcome message and its function
+class UserInterface:
+    #Display the welcome message and its function
+    def intro(self):
+        intro = pyfiglet.figlet_format("WELCOME".center(39, "="), font = "digital")
+        print(intro)
+        print("\033[44;1m" + "This program is a simple app calculator that will ask the user to a math operation and perform it." + "\033[0m")
+        input("Press the ENTER key to run the program....")
+        time.sleep(2)
 #Display the operations
 #Ask the user the operation
 #Ask the user the first number
