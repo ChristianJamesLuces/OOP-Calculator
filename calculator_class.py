f439b12fae820f71d3478e25a2cd4cf322f3e5c8
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-21 21:39:29 +0800

    defining the addition

--- calculator_class.py
+++ calculator_class.py
@@ -3,7 +3,10 @@ class Calculator:
     #Define the Operators
     def __init___(self):
         self.operators = ['+', '-', '/', '*']
-#Define the Addition
+    #Define the Addition
+    def add(self, number1, number2):
+        add = number1 + number2
+        return add
 #Define the Subtraction
 #GDefine the Multiplication
 #Define the Divition
