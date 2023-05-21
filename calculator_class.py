189fd385b358ec4c4a2eebc9180ba85255fcc8c4
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-21 21:51:07 +0800

    defining the division

--- calculator_class.py
+++ calculator_class.py
@@ -15,5 +15,11 @@ class Calculator:
     #Define the Multiplication
     def multiply(self, number1, number2):
         return number1 * number2
-#Define the Divition
+    
+    #Define the Division
+    def devide(self, number1, number2):
+        if number2 != 0:
+            return number1 / number2
+        else:
+            raise ValueError("Invalid Input: Cannot be divide by zero.")
 #Calculate for the result
